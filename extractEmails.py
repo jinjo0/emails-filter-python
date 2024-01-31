2b8d0fca65d20d93436ec00770e34e06de7064c4
KOUKI <64950059+jinjo0@users.noreply.github.com>
2024-01-31 14:10:13 +0000

    Create extractEmails.py

---
+++ extractEmails.py
@@ -0,0 +1,27 @@
+import os
+import re
+
+def extract_emails(input_text):
+    # Regular expression to extract email addresses
+    email_pattern = re.compile(r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b')
+    emails = email_pattern.findall(input_text)
+    return emails
+
+def process_file(input_path):
+    output_path = f"{os.path.splitext(input_path)[0]}_emails.txt"
+
+    with open(input_path, 'r', encoding='utf-8', errors='ignore') as input_file:
+        content = input_file.read()
+        emails = extract_emails(content)
+
+    with open(output_path, 'w') as output_file:
+        for email in emails:
+            output_file.write(f"{email}\n")
+
+if __name__ == "__main__":
+    input_folder = os.path.abspath(os.path.dirname(__file__))  # Current folder where the script is located
+
+    for root, dirs, files in os.walk(input_folder):
+        for file in files:
+            file_path = os.path.join(root, file)
+            process_file(file_path)
